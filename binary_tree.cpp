3b09c5f4c561c170292ffb3887982c50b2f4bf78
mrx <hazema.saadallah@gmail.com>
2023-05-15 03:28:53 +0300

    parents added

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -21,7 +21,7 @@ public:
 
   void preorder_traverse() {
     if (!this->isexternal()) {
-      std::cout << this->data<<'\t';
+      std::cout << this->data << '\t';
       this->left->preorder_traverse();
       this->right->preorder_traverse();
     }
@@ -30,39 +30,45 @@ public:
   void inorder() {
     if (!this->isexternal()) {
       this->left->inorder();
-      std::cout << this->data<<'\t';
+      std::cout << this->data << '\t';
       this->right->inorder();
     }
   }
 };
 
 template <typename T2> node<T2> test_create_tree() {
-  node<double> root(1);
-  root.right = new node<double>(2);
-  root.right->right = new node<double>(3);
-  root.right->left = new node<double>(4);
-  root.left = new node<double>(5);
-  root.left->right = new node<double>(6);
-  root.left->left = new node<double>(7);
-  return root;
+  node<double> *root = new node<double>(1);
+  root->right = new node<double>(2);
+  root->right->parent = root;
+  root->right->right = new node<double>(3);
+  root->right->right->parent = root->right;
+  root->right->left = new node<double>(4);
+  root->right->left->parent = root->right;
+  root->left = new node<double>(5);
+  root->left->parent = root;
+  root->left->right = new node<double>(6);
+  root->left->right->parent = root->left;
+  root->left->left = new node<double>(7);
+  root->left->left->parent = root->left;
+  return *root;
 }
 
 void test_postorder() {
   node<double> root = test_create_tree<double>();
   root.postorder_traverse();
-  std::cout<<std::endl;
+  std::cout << std::endl;
 }
 
 void test_preorder() {
   node<double> root = test_create_tree<double>();
   root.preorder_traverse();
-  std::cout<<std::endl;
+  std::cout << std::endl;
 }
 
 void test_inorder() {
   node<double> root = test_create_tree<double>();
   root.inorder();
-  std::cout<<std::endl;
+  std::cout << std::endl;
 }
 
 int main() {
